43f1a97eafc4e2ad883715cb59e597d6f1f9cff1
Pete Johnson <petecj2@gmail.com>
2022-03-21 14:57:42 -0400

    Changed intance type to micro

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "aws-example" {
 
   region = "us-east-2"
   ami = "ami-0d5d9d301c853a04a"
-  instance_type = "t2.small"
+  instance_type = "t2.micro"
 }
 output "ip_address" {
   description = "IP address of our new instance"
