4671af5cb4dca700d3a62efd23faeb4f58eca290
Pete Johnson <petecj2@gmail.com>
2022-03-21 15:28:29 -0400

    Changed reference to the module to go over HTTPS

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "aws-example" {
-  source = "git@github.com:nerdguru/aws-iac-cicddemo.git"
+  source = "github.com/nerdguru/aws-iac-cicddemo.git"
 
   region = "us-east-2"
   ami = "ami-0d5d9d301c853a04a"
