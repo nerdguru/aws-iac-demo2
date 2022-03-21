9afbe239c62edbd417267fc438f4121b91c46206
Pete Johnson <petecj2@gmail.com>
2022-03-21 14:25:28 -0400

    Added output

---
+++ main.tf
@@ -0,0 +1,11 @@
+module "aws-example" {
+  source = "git@github.com:nerdguru/aws-iac-cicddemo.git"
+
+  region = "us-east-2"
+  ami = "ami-0d5d9d301c853a04a"
+  instance_type = "t2.micro"
+}
+output "ip_address" {
+  description = "IP address of our new instance"
+  value       = module.aws-example.ip_address
+}
